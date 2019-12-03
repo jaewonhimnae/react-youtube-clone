a15d8092365165a520a2b8e80b32a9c64e433e32
John Ahn <smileajw1004@gmail.com>
2019-12-03 23:53:00 +0900

    inital

---
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { FaCode } from "react-icons/fa";
+
+function LandingPage() {
+    return (
+        <>
+        <div className="app">
+            <FaCode style={{ fontSize: '4rem' }} /><br />
+            <span style={{ fontSize: '2rem' }}>Let's Start Coding!</span>
+        </div>
+        <div style={{ float:'right' }}>Thanks For Using This Boiler Plate by John Ahn</div>
+        </>
+    )
+}
+
+export default LandingPage
---
+++ client/src/components/views/UploadVideoPage/UploadVideoPage.js
@@ -0,0 +1,117 @@
+import React, { useState, useEffect} from 'react'
+import { Typography, Button, Form, message, Input, Icon } from 'antd';
+import Dropzone from 'react-dropzone';
+
+const { Title } = Typography;
+const { TextArea } = Input;
+
+const Private = [
+    { value: 0, label:'Private'},
+    { value: 1, label:'Public'}
+]
+
+const Catogory = [
+    { value: 0, label: "Film & Animation" },
+    { value: 0, label: "Autos & Vehicles" },
+    { value: 0, label: "Music" },
+    { value: 0, label: "Pets & Animals" },
+    { value: 0, label: "Sports" },
+]
+
+function UploadVideoPage() {
+
+    const [title, setTitle] = useState("");
+    const [Description, setDescription] = useState("");
+    const [privacy, setPrivacy] = useState(0)
+    const [Categories, setCategories] = useState("Film & Animation")
+
+
+    const handleChangeTitle = ( event ) => {
+        setTitle(event.currentTarget.value)
+    }
+
+    const handleChangeDecsription = (event) => {
+        console.log(event.currentTarget.value)
+
+        setDescription(event.currentTarget.value)
+    }
+
+    const handleChangeOne = (event) => {
+        setPrivacy(event.currentTarget.value)
+    }
+
+    const handleChangeTwo = (event) => {
+        setCategories(event.currentTarget.value)
+    }
+
+    const onSubmit = () => {
+        
+    }
+
+    return (
+        <div style={{ maxWidth: '700px', margin: '2rem auto' }}>
+        <div style={{ textAlign: 'center', marginBottom: '2rem' }}>
+            <Title level={2} > Upload Video</Title>
+        </div>
+
+        <Form onSubmit={onSubmit}>
+            <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+                <Dropzone 
+                    multiple={false}
+                    maxSize={800000000}>
+                    {({ getRootProps, getInputProps }) => (
+                        <div style={{ width: '300px', height: '240px', border: '1px solid lightgray', display: 'flex', alignItems: 'center', justifyContent: 'center' }}
+                            {...getRootProps()}
+                        >
+                            <input {...getInputProps()} />
+                            <Icon type="plus" style={{ fontSize: '3rem' }} />
+
+                        </div>
+                    )}
+                </Dropzone>
+
+                {/* {thumbnail !== "" &&
+                    <div>
+                        <img src={`http://localhost:5000/${thumbnail}`} alt="haha" />
+                    </div>
+                } */}
+            </div>
+
+            <br /><br />
+            <label>Title</label>
+            <Input
+                 onChange={handleChangeTitle}
+                 value={title}
+            />
+            <br /><br />
+            <label>Description</label>
+            <TextArea
+                 onChange={handleChangeDecsription}
+                 value={Description}
+            />
+            <br /><br />
+
+            <select onChange={handleChangeOne}>
+                {Private.map((item, index) => (
+                    <option key={index} value={item.value}>{item.label}</option>
+                ))}
+            </select>
+            <br /><br />
+
+            <select onChange={handleChangeTwo}>
+                {Catogory.map((item, index) => (
+                    <option key={index} value={item.label}>{item.label}</option>
+                ))}
+            </select>
+            <br /><br />
+
+            <Button type="primary" size="large" onClick={onSubmit}>
+                Submit
+            </Button>
+
+        </Form>
+    </div>
+    )
+}
+
+export default UploadVideoPage
