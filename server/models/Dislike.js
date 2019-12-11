e0f82fd7f47bb2f88deac8c386184f1b71eaa8e0
John Ahn <smileajw1004@gmail.com>
2019-12-11 23:34:36 +0900

    Added DisLike & Like Mode

---
+++ server/models/Dislike.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const dislikeSchema = mongoose.Schema({
+   userId: {
+       type: Schema.Types.ObjectId,
+       ref: 'User'
+   },
+   commentId: {
+       type: Schema.Types.ObjectId,
+       ref: 'Comment'
+   },
+   videoId: {
+       type: Schema.Types.ObjectId,
+       ref: 'Video'
+   }
+
+}, { timestamps: true })
+
+
+const Dislike = mongoose.model('Dislike', dislikeSchema);
+
+module.exports = { Dislike }
